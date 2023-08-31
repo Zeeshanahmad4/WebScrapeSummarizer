dd0448ca1fda3819a81c4f206f47946e87cffb22
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 12:56:42 +0500

    Update openai.php

--- libs/openai.php
+++ libs/openai.php
@@ -1,1 +1,18 @@
-// OpenAI PHP library code goes here 
+<?php
+function processWithOpenAI($content) {
+    $openai_api_key = "YOUR_OPENAI_API_KEY";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/engines/davinci/completions");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(["prompt" => $content]));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $openai_api_key,
+        'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $decodedResponse = json_decode($response, true);
+    return $decodedResponse['choices'][0]['text'];
+}
+?>
