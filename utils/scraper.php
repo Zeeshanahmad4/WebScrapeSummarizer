57b525a26d6a6628c072bbd254f115fed3cdcf31
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 12:57:36 +0500

    Update scraper.php

--- utils/scraper.php
+++ utils/scraper.php
@@ -1,1 +1,10 @@
-// Web scraper code goes here 
+<?php
+function scrapeContent($domain) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $domain);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content;
+}
+?>
