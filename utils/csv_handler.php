0d19eacc401ee412b85ed72e5d98719e2b269a49
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 12:57:13 +0500

    Update csv_handler.php

--- utils/csv_handler.php
+++ utils/csv_handler.php
@@ -1,1 +1,9 @@
-// CSV handler code goes here 
+<?php
+function writeToCSV($data, $filename = "output.csv") {
+    $file = fopen($filename, 'w');
+    foreach ($data as $row) {
+        fputcsv($file, $row);
+    }
+    fclose($file);
+}
+?>
